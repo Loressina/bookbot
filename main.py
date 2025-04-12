56fb42737401322362f6d178a7020f34fed6d60e
Loressina <loressina@posteo.de>
2025-04-13 01:10:01 +0200

    add error handling

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from stats import count_characters
 from stats import sort_char_counts
 
 def get_book_text(filepath):
-    with open(filepath) as file:
-        return file.read()
+    try:
+        with open(filepath) as file:
+            return file.read()
+    except Exception as e:
+        print(f"Error reading file '{filepath}': {e}")
+        sys.exit(1)
 
 def print_report(filepath, word_count, char_counts):
     print("============ BOOKBOT ============")
